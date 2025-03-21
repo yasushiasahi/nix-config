a4fb226c2f2c27a5af776cd82efd6270a3b35333
Yasushi Asahi <asahi1600@gmail.com>
2025-03-21 14:32:58 +0900

    darwin ctl+spaceのショートカット無効化が上手く行ってないので諦める

--- darwin.nix
+++ darwin.nix
@@ -112,20 +112,6 @@
       Sound = true;
       Display = true;
     };
-    CustomUserPreferences = {
-      "com.apple.symbolichotkeys" = {
-        AppleSymbolicHotKeys = {
-          # Disable 'Cmd + Space' for Spotlight Search
-          "64" = {
-            enabled = false;
-          };
-          # Disable 'Cmd + Alt + Space' for Finder search window
-          "65" = {
-            enabled = false;
-          };
-        };
-      };
-    };
     WindowManager.EnableStandardClickToShowDesktop = false;
   };
 
