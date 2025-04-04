6112cf1077b8d4225f6437192c5dbdce5e62c061
Yasushi Asahi <asahi1600@gmail.com>
2025-04-04 12:44:01 +0900

    darwin-rebuildのabbr修正

--- home-manager/nix/default.nix
+++ home-manager/nix/default.nix
@@ -6,7 +6,7 @@
       home-manager.enable = true;
     }
     // mkAbbr {
-      dasw = "darwin-rebuild switch --flake ~/ghq/github.com/yasushiasahi/nix-config#darwin";
+      drsw = "darwin-rebuild switch --flake ~/ghq/github.com/yasushiasahi/nix-config#darwin";
       hmsw = "home-manager switch --flake ~/ghq/github.com/yasushiasahi/nix-config#home";
       dilo = "nix flake init -t ~/ghq/github.com/yasushiasahi/nix-config#dir-locals";
       gtexdr = "echo \".dir-locals.el\\n.dir-locals.nix\\n.envrc\" >> .git/info/exclude && git update-index --assume-unchanged .dir-locals.el .dir-locals.nix .envrc";
