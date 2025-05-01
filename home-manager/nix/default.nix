b075714fe5b95317585cc1a5a429cf2c04383794
Yasushi Asahi <asahi1600@gmail.com>
2025-05-02 00:44:30 +0900

    home-manager タイポ修正

--- home-manager/nix/default.nix
+++ home-manager/nix/default.nix
@@ -8,7 +8,7 @@
     // mkAbbr {
       drsw = "darwin-rebuild switch --flake ~/ghq/github.com/yasushiasahi/nix-config#darwin";
       hmsw = "home-manager switch --flake ~/ghq/github.com/yasushiasahi/nix-config#home";
-      hmne = "home-manager new --flake ~/ghq/github.com/yasushiasahi/nix-config#home";
+      hmne = "home-manager news --flake ~/ghq/github.com/yasushiasahi/nix-config#home";
       dilo = "nix flake init -t ~/ghq/github.com/yasushiasahi/nix-config#dir-locals";
       gtexdr = "echo \".dir-locals.el\\n.dir-locals.nix\\n.envrc\" >> .git/info/exclude && git update-index --assume-unchanged .dir-locals.el .dir-locals.nix .envrc";
     };
