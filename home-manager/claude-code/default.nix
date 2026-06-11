507bf86c62334e4cfc965b01cee8bc68515ca2e9
Yasushi Asahi <asahi1600@gmail.com>
2026-06-11 09:49:30 +0900

    update

--- home-manager/claude-code/default.nix
+++ home-manager/claude-code/default.nix
@@ -1,33 +1,44 @@
-{ pkgs, ... }:
+{ ... }:
+
 {
   programs.claude-code = {
     enable = true;
-    lspServers = {
-      astro = {
-        # command = "astro-ls";
-        # args = [ "--stdio" ];
-        command = "npx";
-        args = [
-          "@astrojs/language-server"
-          "--stdio"
-        ];
-        extensionToLanguage = {
-          ".astro" = "astro";
-        };
-        initializationOptions = {
-          typescript = {
-            tsdk = "${pkgs.typescript}/lib/node_modules/typescript/lib/";
-          };
-        };
-      };
-
-      nix = {
-        command = "nixd";
-        args = [ "--stdio" ];
-        extensionToLanguage = {
-          ".nix" = "nix";
-        };
-      };
-    };
   };
+
+  # home.file.".claude/plugins/.lsp.json" = {
+  #   text = builtins.toJSON {
+  #     astro = {
+  #       command = "npx";
+  #       args = [
+  #         "@astrojs/language-server"
+  #         "--stdio"
+  #       ];
+  #       extensionToLanguage = {
+  #         ".astro" = "astro";
+  #       };
+  #       initializationOptions = {
+  #         typescript = {
+  #           tsdk = "${pkgs.typescript}/lib/node_modules/typescript/lib/";
+  #         };
+  #       };
+  #     };
+
+  #     terraform = {
+  #       command = "terraform-ls";
+  #       args = [ "serve" ];
+  #       extensionToLanguage = {
+  #         ".tf" = "tf";
+  #         ".hcl" = "hcl";
+  #       };
+  #     };
+
+  #     nix = {
+  #       command = "nixd";
+  #       args = [ "--stdio" ];
+  #       extensionToLanguage = {
+  #         ".nix" = "nix";
+  #       };
+  #     };
+  #   };
+  # };
 }
--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -1,35 +1,5 @@
 { pkgs, ... }:
-let
-  # tmux起動時にfishを使う。設定しないデフォルトシェル(zsh)が使われてしまう。
-  extraConfigDefaultShell = ''
-    set-option -g default-shell ${pkgs.fish}/bin/fish
-  '';
 
-  extraConfigReroadConfig = ''
-    bind r source-file "$XDG_CONFIG_HOME/tmux/tmux.conf" \; display "Reloaded!"
-    set -g status-position top
-  '';
-
-  extraConfigStatusLine = ''
-    # 左側の長さ(デフォルトは10)
-    set -g status-left-length 50
-    # ウィンドウリストを中央配置
-    set-option -g status-justify centre
-  '';
-
-  # フルカラーサポート
-  # https://apribase.net/2025/05/28/term-terminfo/
-  extraConfigTerminalFeatures = ''
-    set -as terminal-features ",alacritty:RGB,xterm-ghostty:RGB,foot:RGB,wezterm:RGB"
-  '';
-
-  # Claude CodeでShift+Enterで改行できるようにする
-  # https://blog.bobuhiro11.net/en/2026/02-27-shift-enter.html
-  extraConfigClaudeCode = ''
-    bind-key -n S-Enter send-keys -l "[13;2u"
-  '';
-
-in
 {
   programs.tmux = {
     enable = true;
@@ -41,31 +11,41 @@ in
     keyMode = "emacs";
     mouse = true;
     prefix = "C-t";
-    terminal = "screen-256color";
-
     plugins = [
       pkgs.tmuxPlugins.pain-control
-      {
-        plugin = pkgs.tmuxPlugins.tmux-colors-solarized;
-        extraConfig = ''
-          set -g @colors-solarized 'dark'
-        '';
-      }
+      # {
+      #   plugin = pkgs.tmuxPlugins.tmux-colors-solarized;
+      #   extraConfig = ''
+      #     set -g @colors-solarized 'dark'
+      #   '';
+      # }
     ];
-    extraConfig =
-      extraConfigDefaultShell
-      + extraConfigReroadConfig
-      + extraConfigStatusLine
-      + extraConfigTerminalFeatures
-      + extraConfigClaudeCode;
+    extraConfig = ''
+      # tmux起動時にfishを使う。設定しないとデフォルトシェル(zsh)が使われてしまう。
+      set-option -g default-shell ${pkgs.fish}/bin/fish
+
+      # C-t rでコンフィグを再読み込みする
+      bind r source-file "$XDG_CONFIG_HOME/tmux/tmux.conf" \; display "Reloaded!"
+
+      # ステータスラインの設定
+      set -g status-position top           # 上部に表示      
+      set -g status-left-length 50         # 左側の長さ(デフォルトは10)      
+      set-option -g status-justify centre  # ウィンドウリストを中央配置
+
+      # フルカラーサポート。Alacritty + tmux情報がありすぎて決定版がわからない。暇があれば色々試す。
+      # https://apribase.net/2025/05/28/term-terminfo/
+      # https://zenn.dev/a24k/articles/20221027-alacritty-tmux
+      set -g default-terminal "screen-256color"
+      set -as terminal-features ",alacritty:RGB"
+      set -ag terminal-overrides ",alacritty:RGB"
+
+      # Claude CodeでShift+Enterで改行できるようにする。
+      # https://blog.bobuhiro11.net/en/2026/02-27-shift-enter.html
+      bind-key -n S-Enter send-keys -l "[13;2u"
+    '';
   };
 
-  # セッションをいい感じに扱える
-  # https://github.com/joshmedeski/sesh
-  programs.sesh = {
-    enable = true;
-    icons = true;
-    tmuxKey = "S";
-    enableTmuxIntegration = true;
-  };
+  home.packages = [
+    pkgs.sesh
+  ];
 }
