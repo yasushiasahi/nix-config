0fd079339bcbf9ea14b9c1925c1a2bb4df9a05b7
Yasushi Asahi <asahi1600@gmail.com>
2026-06-17 15:26:50 +0900

    不要フォルダ削除

--- home-manager/colima/default.nix
+++
@@ -1,5 +0,0 @@
-{ config, ... }:
-{
-  xdg.configFile."colima/default/colima.yaml".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.nix-config}/home-manager/colima/default/colima.yaml";
-}
