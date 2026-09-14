f3f99c3f613bc5cc2b1f91b7362b9259ab2bb726
Yasushi Asahi <asahi1600@gmail.com>
2026-09-14 16:28:48 +0900

    fishの設定。home-managerのバグ。

--- home-manager/fish/default.nix
+++ home-manager/fish/default.nix
@@ -12,10 +12,13 @@
     shellAliases = {
       rm = "trash";
     };
-    binds = {
-      "ctrl-t".command = "tv";
-      "ctrl-j".command = "tv_smart_autocomplete";
-    };
+    # NOTE: programs.fish.binds は home-manager 側の型定義が
+    # `type // { check = ...; }` を使っており、nixpkgs の module system v2 merge
+    # と非互換でエラーになるため、生成結果と同等の関数を直接定義している
+    functions.fish_user_key_bindings = ''
+      bind ctrl-t 'tv'
+      bind ctrl-j 'tv_smart_autocomplete'
+    '';
   };
 
   # trace: warning: programs.man.generateCaches has no effect when programs.man.package is null を抑制するため
